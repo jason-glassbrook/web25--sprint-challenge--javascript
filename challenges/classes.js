a3c639ac5d07975a978adbeef0997554a82ea1e4
o1123516766542617 <o1123516766542617@outlook.com>
2019-10-18 12:28:30 -0400

    style comments (probably a waste of time...)

--- challenges/classes.js
+++ challenges/classes.js
@@ -6,10 +6,18 @@ function heading (content) {
   console.log (`\n### ${content} ###\n`);
 }
 
-// 1. Copy and paste your prototype in here and refactor into class syntax.
+/***************************************
+  Task 1
+  ------------------------------------
+  Copy and paste your prototype in here and refactor into class syntax.
+***************************************/
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+/***************************************
+  Stretch
+  ------------------------------------
+  Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+***************************************/
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,7 +6,9 @@ function heading (content) {
   console.log (`\n### ${content} ###\n`);
 }
 
-// ==== Objects ====
+/***************************************
+  Objects
+***************************************/
 
 /* 
   Given the following information about dinosaurs, create 3 objects: 
@@ -34,12 +36,12 @@ console.log();
 // What time period did tyrannosaurus live in?
 console.log();
 
-
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
 console.log();
 
-
-// ==== Arrays ====
+/***************************************
+  Arrays
+***************************************/
 
 // Given an array of college graduates.  Complete the following requests using any array method you like
 
@@ -56,27 +58,43 @@ const graduates = [
   { id: 10, first_name: "Hube", university: "Universitat Rovira I Virgili Tarragona", email: "hlethbrig9@foxnews.com" },
 ];
 
-/* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
+/*******************
+  Request 1
+  ----------------
+  Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
-Once you have the new array created, sort the universities alphabetically and log the result. */
+  Once you have the new array created, sort the universities alphabetically and log the result.
+*******************/
+
 const universities = [];
 console.log(universities);
 
-/* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
+/*******************
+  Request 2
+  ----------------
+  Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
+  
+  The resulting contact information strings should have a space between the first name and the email, like this: 
+  "Josh josh@example.com"
 
-The resulting contact information strings should have a space between the first name and the email, like this: 
-"Josh josh@example.com"
+  Log the result of your new array. 
+*******************/
 
-Log the result of your new array. */
 const contactInfo = [];
 console.log(contactInfo);
 
-/* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
+/*******************
+  Request 3
+  ----------------
+  Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. 
+*******************/
+
 const unisWithUni = [];
 console.log(unisWithUni);
 
-
-// ==== ADVANCED Array Methods ====
+/***************************************
+  ADVANCED Array Methods
+***************************************/
 
 // Given this zoo data from around the United States, follow the instructions below.  Use the specific array methods in the requests below to solve the problems.
 
@@ -93,43 +111,44 @@ const zooAnimals = [
   { animal_name: "Australian pelican", population: 5, scientific_name: "Pelecanus conspicillatus", state: "West Virginia" },
 ];
 
-/* Request 1: .forEach()
+/*******************
+  Request 1: .forEach()
+  ----------------
+  The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
+*******************/
 
-The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
-
-*/
 const displayNames = [];
 console.log(displayNames);
 
-/* Request 2: .map()
-
-The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
-
-*/
+/*******************
+  Request 2: .map()
+  ----------------
+  The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
+*******************/
 
 const lowCaseAnimalNames = [];
 console.log(lowCaseAnimalNames);
 
-/* Request 3: .filter() 
+/*******************
+  Request 3: .filter() 
+  ----------------
+  The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
+*******************/
 
-The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
-
-*/
 const lowPopulationAnimals = [];
 console.log(lowPopulationAnimals);
 
-/* Request 4: .reduce() 
+/*******************
+  Request 4: .reduce() 
+  ----------------
+  The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+*******************/
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
-
-*/
 const populationTotal = 0;
 console.log(populationTotal);
 
-
-/*
-
-Stretch: If you haven't already, convert your array method callbacks into arrow functions.
-
-*/
-
+/*******************
+  STRETCH
+  ----------------
+  If you haven't already, convert your array method callbacks into arrow functions.
+*******************/
