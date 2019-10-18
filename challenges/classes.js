0eaab8e43803dee04d2909661324caaff2e7b28c
o1123516766542617 <o1123516766542617@outlook.com>
2019-10-18 13:28:01 -0400

    do classes/task-1: convert prototype to class

--- challenges/classes.js
+++ challenges/classes.js
@@ -14,9 +14,47 @@ function heading (content , before = 1, after = 1) {
   Copy and paste your prototype in here and refactor into class syntax.
 ***************************************/
 
+heading ("CLASSES");
+
+class CuboidMaker
+{
+  constructor (attrs)
+  {
+    this.length = attrs.length;
+    this.width  = attrs.width;
+    this.height = attrs.height;
+  }
+  volume () {
+    return (this.length * this.width * this.height);
+  }
+  surfaceArea () {
+    return (
+      2 * (
+        (this.length * this.width)
+      + (this.length * this.height)
+      + (this.width  * this.height)
+      )
+    );
+  }
+}
+
+/*******************
+  testing
+*******************/
+
+const cuboid = new CuboidMaker ({
+  length : 4,
+  width  : 5,
+  height : 5
+});
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log (cuboid);
+// >>> CuboidMaker {...}
+console.log (cuboid.volume ());
+// >>> 100
+console.log (cuboid.surfaceArea ());
+// >>> 130
 
 /***************************************
   Stretch
